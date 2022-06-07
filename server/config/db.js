fe23589e0bb53afe7c7ba41106fb948263121eb6
Akhil K <akhilkponnanivj45@gmail.com>
2022-06-07 13:53:57 +0530

    Updated default dbname in backend.

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/chat-app"
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/food-menu"
 const connectDB = mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
